7e78f778c70ead8ef2e004bfa37642e07ee9524d
Yara <yarra.hani2003@gmail.com>
2025-08-01 22:52:31 +0300

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,6 @@
+using API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
 namespace API.Entities;
 
 public class AppUser
@@ -5,4 +8,7 @@ public class AppUser
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string DisplayName { get; set; }
     public required string Email { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
+
 }
